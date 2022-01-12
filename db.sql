ef468ea29bc13d6652db561a7d95537b12124e68
Adrian Șendroiu <molecula2788@gmail.com>
2022-01-12 12:43:05 +0200

    add default db.sql for mariadb

--- db.sql
+++ db.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.13  Distrib 8.0.27, for Linux (x86_64)
+-- MariaDB dump 10.19  Distrib 10.7.1-MariaDB, for debian-linux-gnu (x86_64)
 --
 -- Host: localhost    Database: bot
 -- ------------------------------------------------------
--- Server version	8.0.27
+-- Server version	10.7.1-MariaDB-1:10.7.1+maria~focal
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -21,25 +21,16 @@
 
 DROP TABLE IF EXISTS `bet_choices`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `bet_choices` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `bet_id` int unsigned NOT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `bet_id` int(10) unsigned NOT NULL,
   `choice` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `bet_id` (`bet_id`),
   CONSTRAINT `bet_choices_ibfk_1` FOREIGN KEY (`bet_id`) REFERENCES `bets` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `bet_choices`
---
-
-LOCK TABLES `bet_choices` WRITE;
-/*!40000 ALTER TABLE `bet_choices` DISABLE KEYS */;
-/*!40000 ALTER TABLE `bet_choices` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `bet_questions`
@@ -47,25 +38,16 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `bet_questions`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `bet_questions` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `bet_id` int unsigned NOT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `bet_id` int(10) unsigned NOT NULL,
   `question` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `bet_id` (`bet_id`),
   CONSTRAINT `bet_questions_ibfk_1` FOREIGN KEY (`bet_id`) REFERENCES `bets` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `bet_questions`
---
-
-LOCK TABLES `bet_questions` WRITE;
-/*!40000 ALTER TABLE `bet_questions` DISABLE KEYS */;
-/*!40000 ALTER TABLE `bet_questions` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `bet_votes`
@@ -73,27 +55,18 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `bet_votes`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `bet_votes` (
-  `bet_id` int unsigned NOT NULL,
+  `bet_id` int(10) unsigned NOT NULL,
   `user` varchar(255) NOT NULL,
-  `ts` bigint NOT NULL,
-  `choice_id` int unsigned NOT NULL,
+  `ts` bigint(20) NOT NULL,
+  `choice_id` int(10) unsigned NOT NULL,
   PRIMARY KEY (`bet_id`,`user`),
   KEY `bet_id` (`bet_id`,`choice_id`),
   CONSTRAINT `bet_votes_ibfk_1` FOREIGN KEY (`bet_id`) REFERENCES `bets` (`id`),
   CONSTRAINT `bet_votes_ibfk_2` FOREIGN KEY (`bet_id`, `choice_id`) REFERENCES `bet_choices` (`bet_id`, `id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `bet_votes`
---
-
-LOCK TABLES `bet_votes` WRITE;
-/*!40000 ALTER TABLE `bet_votes` DISABLE KEYS */;
-/*!40000 ALTER TABLE `bet_votes` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `bet_votes_history`
@@ -101,27 +74,18 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `bet_votes_history`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `bet_votes_history` (
-  `bet_id` int unsigned NOT NULL,
+  `bet_id` int(10) unsigned NOT NULL,
   `user` varchar(255) NOT NULL,
-  `ts` bigint NOT NULL,
-  `choice_id` int unsigned NOT NULL,
+  `ts` bigint(20) NOT NULL,
+  `choice_id` int(10) unsigned NOT NULL,
   PRIMARY KEY (`bet_id`,`user`,`ts`),
   KEY `bet_id` (`bet_id`,`choice_id`),
   CONSTRAINT `bet_votes_history_ibfk_1` FOREIGN KEY (`bet_id`) REFERENCES `bets` (`id`),
   CONSTRAINT `bet_votes_history_ibfk_2` FOREIGN KEY (`bet_id`, `choice_id`) REFERENCES `bet_choices` (`bet_id`, `id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `bet_votes_history`
---
-
-LOCK TABLES `bet_votes_history` WRITE;
-/*!40000 ALTER TABLE `bet_votes_history` DISABLE KEYS */;
-/*!40000 ALTER TABLE `bet_votes_history` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `bets`
@@ -129,30 +93,21 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `bets`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `bets` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `user` varchar(255) NOT NULL,
-  `ts` bigint NOT NULL,
-  `resolve_date` bigint NOT NULL,
-  `active` int NOT NULL,
-  `correct_choice_id` int unsigned DEFAULT NULL,
+  `ts` bigint(20) NOT NULL,
+  `resolve_date` bigint(20) NOT NULL,
+  `active` int(11) NOT NULL,
+  `correct_choice_id` int(10) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `id` (`id`,`correct_choice_id`),
   CONSTRAINT `bets_ibfk_1` FOREIGN KEY (`id`, `correct_choice_id`) REFERENCES `bet_choices` (`bet_id`, `id`),
-  CONSTRAINT `bets_chk_2` CHECK ((`active` <= 1)),
-  CONSTRAINT `bets_chk_3` CHECK ((((`active` = 0) and (`correct_choice_id` is not null)) or ((`active` = 1) and (`correct_choice_id` is null))))
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  CONSTRAINT `bets_chk_2` CHECK (`active` <= 1),
+  CONSTRAINT `bets_chk_3` CHECK (`active` = 0 and `correct_choice_id` is not null or `active` = 1 and `correct_choice_id` is null)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `bets`
---
-
-LOCK TABLES `bets` WRITE;
-/*!40000 ALTER TABLE `bets` DISABLE KEYS */;
-/*!40000 ALTER TABLE `bets` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `bot_config`
@@ -160,24 +115,30 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `bot_config`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `bot_config` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(128) DEFAULT NULL,
   `value` varchar(128) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `bot_config`
+-- Table structure for table `users`
 --
 
-LOCK TABLES `bot_config` WRITE;
-/*!40000 ALTER TABLE `bot_config` DISABLE KEYS */;
-/*INSERT INTO `bot_config` VALUES (1,'channel_id','C02QWRJGHL2')*/;
-/*!40000 ALTER TABLE `bot_config` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` varchar(255) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `avatar_url` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -188,4 +149,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-01-02 14:18:07
+-- Dump completed on 2022-01-12 10:22:59
